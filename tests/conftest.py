29c9a42c6dea9b5ddf2bfb20660bec851274110e
Sergey Lvov <Lvov.Sergey.V@yandex.ru>
2025-02-09 16:04:15 +0300

    Поправил фикстур create_courier так как он использовал дважды удаление, добавил логирование какие данные фикстур отправляет, какие видет после теста и какие использует для удаления

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 import pytest
 import random
 import string
+import logging
 from api_client.api_client import APIClient
 from settings import Settings as St
 
-
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @pytest.fixture
 def api_client():
@@ -28,16 +30,17 @@ def delete_courier(api_client):
         "password": password,
         "firstName": first_name
     }
-
+    logger.info(f"Выданные данные для регистрации: {payload}") #Логирование выдаваемых данных
     # Возвращаем данные курьера тесту
     yield payload
-
+    logger.info(f"Измененные данные в тесте: {payload}") #Логирование какие данные возвращаются после теста
     # Удаляем курьера после завершения теста
     login_payload = {
         "login": login,
         "password": password
     }
     # После завершения фикстур удаляем курьера
+    logger.info(f"Авторизация курьера для удаления: {login_payload}") #Логирование данных которые используются для удаления
     response_login = api_client.post(St.ENDPOINT_AUTH_COURIER, login_payload )
     if response_login.status_code == 200:
         id_courier = response_login.json()['id']
@@ -53,10 +56,4 @@ def create_courier(api_client, delete_courier):
 
     del payload["firstName"]
     # Возвращаем данные для авторизации
-    yield payload
-
-    # Удаляем курьера после завершения теста
-    response_login = api_client.post(St.ENDPOINT_AUTH_COURIER, payload )
-    if response_login.status_code == 200:
-        id_courier = response_login.json()['id']
-        api_client.delete(f'{St.ENDPOINT_COURIER}/{id_courier}')
+    return payload
